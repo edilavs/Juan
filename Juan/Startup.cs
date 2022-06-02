2eadac93f2a718a42eb89a2369e178a41550aafb
unknown <7nuv1d1@code.edu.az>
2022-06-02 16:51:03 +0400

    added task

---
+++ Juan/Startup.cs
@@ -0,0 +1,46 @@
+using Juan.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan
+{
+    public class Startup
+    {
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer("server=DESKTOP-GCUJ6KM;Database=BP202JUAN;Trusted_connection=TRUE");
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=home}/{action=index}/{id?}"
+                    );
+            });
+        }
+    }
+}
